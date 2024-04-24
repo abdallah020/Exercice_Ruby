fb6d05ace537aade84a5e46780ea3e0572f5135c
Abdallah020 <mbackeboune@gmail.com>
2024-04-24 11:00:42 +0000

    exo9

---
+++ exo_09.rb
@@ -0,0 +1,14 @@
+puts "Bonjour, c'est quoi ton prenom ?"
+
+print "> "
+
+user_name = gets.chomp
+
+puts "Et ton nom ?"
+
+print "> "
+
+user_name2 = gets.chomp
+
+
+puts "Bonjoure, "+user_name+" "+user_name2
